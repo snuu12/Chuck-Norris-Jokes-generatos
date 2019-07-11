bb78b1c28736750d61a8bf29f5d01dfafcf2cec3
Michal Snopek <snuu12@gmail.com>
2019-07-11 10:06:42 +0200

    add project Jokes generator

---
+++ src/main/java/kurs/Jokes/JokesControler.java
@@ -0,0 +1,28 @@
+package kurs.Jokes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class JokesControler {
+
+    /*po adresie url:
+    localhost:8080 /przechodzi do metody generateJoke()
+    -ModelMp słuzy do przekazywania danych z Kontrolera do widoku html
+    jeśli piszemy map.put("name","Michał ");
+    to w html będzie widzoczna zmienna name , której wartość będzie Michał
+
+
+
+*/
+    @GetMapping("/")
+    public String generateJoke(ModelMap map){
+        ChuckNorrisQuotes joke =new ChuckNorrisQuotes();
+
+        map.put("Quote",joke.getRandomQuote());
+        return "joke"; //przejdz do pliku://resources/templates/joke.html
+
+    }
+}
